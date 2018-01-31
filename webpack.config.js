fdf025c0105021c252580b6c0e3861ef6ed25e38
Camilo Uribe <milouri23@gmail.com>
2018-01-31 14:28:29 -0500

    Add Sass support (linter and compiler)

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const styleLintPlugin = require('stylelint-webpack-plugin');
 const path = require('path')
 
 module.exports = {
   entry: './src/main.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              'css-loader?sourceMap',
+              'sass-loader'
+            ]
+          })
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new ExtractTextPlugin("styles.css"),
+    new styleLintPlugin({
+      configFile: '.stylelintrc',
+      context: 'src',
+      files: '**/*.scss',
+      failOnError: false,
+      quiet: false,
     })
   ]
 }
