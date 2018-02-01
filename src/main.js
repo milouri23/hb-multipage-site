b48278664ed1eba23253cfa3e1aa2da734385c70
Camilo Uribe Diaz <milouri23@gmail.com>
2018-02-01 14:34:15 -0500

    Divide in two environments. Development and Production

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import './style.scss'
 import MainNav from './modules/main-nav/MainNav.js'
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!')
+}
+
 /* eslint-disable */
 var mainNav = new MainNav(document.querySelector('.main-nav'))
 /* eslint-enable */
